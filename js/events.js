c207ffe7e6ea6e843c3a46dd32eed4c9ad7f516d
Charlie Cook <turingbombe@gmail.com>
2016-09-01 08:58:00 -0400

    Played with event listeners

--- js/events.js
+++ js/events.js
@@ -3,5 +3,36 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+submitIt();
+pressIt();
 });
+  function getIt(){
+    $('p').on('click', function(){
+      alert('Hey!')
+    });
+  }
+
+  function frameIt(){
+    $('img').on('load', function(){
+      $(this).css('border', "solid 2px red");
+      $(this).addClass("tasty");
+    })
+  }
+
+  function submitIt(){
+    $('form').on('submit', function(){
+        alert('Your form is going to be submitted now.');
+        return;
+    })
+  }
+
+  function pressIt(){
+    $('#typing').on('keydown',function(key){
+      if(key.which == 71){
+        alert("You're a G")
+        return
+      }
+    })
+  }
